e3daf5a24176adbf71a4c11f3ba3f2d339f12c77
Thijs Janzen <janzen@evolbio.mpg.de>
2017-11-02 15:32:06 +0100

    added extra Poisson checks

--- tests/testthat/test-isoSim.R
+++ tests/testthat/test-isoSim.R
@@ -15,10 +15,27 @@ test_that("sim_inf_chrom use", {
                 seed = 42)
   
   
+  #test with a number of markers
   sim_inf_chrom(popSize = N, 
                 Hzero = H0, 
                 maxTime = maxT, 
                 size_in_Morgan = M, 
                 markers = 1000, 
+                seed = 42)
+  
+  #test with extremely high recombination rate
+  sim_inf_chrom(popSize = N, 
+                Hzero = H0, 
+                maxTime = maxT, 
+                size_in_Morgan = 20, 
+                markers = -1, 
+                seed = 42)
+  
+  #test with extremely low recombination rate
+  sim_inf_chrom(popSize = N, 
+                Hzero = H0, 
+                maxTime = maxT, 
+                size_in_Morgan = 1e-7, 
+                markers = -1, 
                 seed = 42)
 })
