5b1cfc131b9a9144507bd506b2f5655c3a8c10f0
Thijs Janzen <janzen@evolbio.mpg.de>
2018-03-27 09:50:41 +0200

    added two_alleles support

---
+++ src/selection.h
@@ -0,0 +1,33 @@
+//
+//  Fish.hpp
+//  
+//
+//  Created by Thijs Janzen on 02/11/2017.
+//
+//
+
+#ifndef selection_hpp
+#define selection_hpp
+
+#include <vector>
+#include <algorithm>
+
+#include "Fish.h"
+#include "main.h"
+
+
+#include <Rcpp.h>
+using namespace Rcpp;
+
+int draw_prop_fitness(const std::vector<double> fitness,
+                      double maxFitness);
+
+double calculate_fitness_markers(const Fish& focal,
+                                 const NumericMatrix& select);
+
+NumericVector update_frequency(const std::vector< Fish >& v, double m, int num_alleles) {
+    NumericVector freq(num_alleles);
+
+
+
+#endif /* Fish_hpp */
