bd5f676618eb1639d999b65244b8425d96595749
Thijs Janzen <janzen@evolbio.mpg.de>
2018-04-03 19:39:06 +0200

    added create_tibble_from_freq_mat

--- R/select_population.R
+++ R/select_population.R
@@ -44,6 +44,24 @@ create_tibble_from_freq_table <- function(frequencies, select_matrix) {
   return(freq_tibble)
 }
 
+create_tibble_from_freq_mat <- function(frequencies, select_matrix) {
+  found_markers <- c()
+  for(i in 1:(dim(frequencies)[[1]])) {
+    local_mat <- frequencies[i,]
+    time <- 0
+    marker_indicator <- select_matrix[i, 1]
+    freq_tibble <- c(time, marker_indicator, local_mat)
+
+    found_markers <- rbind(found_markers, freq_tibble)
+  }
+  colnames(found_markers) <- c("time", "location", 0:(length(frequencies[1,])-1))
+  found_markers <- as.tibble(found_markers)
+  found_markers <- gather(found_markers, key = "ancestor", value = "frequency",
+                          -c(1,2))
+
+  return(found_markers)
+}
+
 
 
 
@@ -80,9 +98,9 @@ create_population_selection <- function(pop_size,
                                                  track_frequency)
   popstruct <- create_pop_class(pop$population)
 
-  initial_freq_tibble <- create_tibble_from_freq_table(pop$initial_frequencies,
+  initial_freq_tibble <- create_tibble_from_freq_mat(pop$initial_frequencies,
                                                        select_matrix)
-  final_freq_tibble <- create_tibble_from_freq_table(pop$final_frequencies,
+  final_freq_tibble <- create_tibble_from_freq_mat(pop$final_frequencies,
                                                      select_matrix)
 
 
@@ -141,9 +159,9 @@ select_population <- function(source_pop,
 
   selected_popstruct <- create_pop_class(selected_pop$population)
 
-  initial_freq_tibble <- create_tibble_from_freq_table(selected_pop$initial_frequencies,
+  initial_freq_tibble <- create_tibble_from_freq_mat(selected_pop$initial_frequencies,
                                                        select_matrix)
-  final_freq_tibble <- create_tibble_from_freq_table(selected_pop$final_frequencies,
+  final_freq_tibble <- create_tibble_from_freq_mat(selected_pop$final_frequencies,
                                                      select_matrix)
 
   output <- list("population" = popstruct,
