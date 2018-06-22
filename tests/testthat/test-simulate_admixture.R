dcdb79b01b6daadbc718457e802fcc0195ef6170
Thijs Janzen <thijsjanzen@gmail.com>
2018-06-22 16:58:59 +0200

    added tests

---
+++ tests/testthat/test-simulate_admixture.R
@@ -0,0 +1,78 @@
+context("simulate_Admixture")
+
+test_that("simulate admixture abuse", {
+
+
+
+
+  vx <- simulate_admixture(pop_size = 100,
+                           number_of_founders = 2,
+                           total_runtime = 100,
+                           morgan = 1,
+                           seed = 42,
+                           select_matrix = NA,
+                           progress_bar = TRUE,
+                           track_junctions = FALSE,
+                           track_frequency = FALSE,
+                           multiplicative_selection = TRUE)
+
+  select_matrix <- matrix(NA, nrow=1,ncol=5)
+  testthat::expect_error(simulate_admixture(pop_size = 100,
+                                            number_of_founders = 2,
+                                            total_runtime = 100,
+                                            morgan = 1,
+                                            seed = 42,
+                                            select_matrix = select_matrix,
+                                            progress_bar = TRUE,
+                                            track_junctions = FALSE,
+                                            track_frequency = FALSE,
+                                            multiplicative_selection = TRUE))
+
+  select_matrix <- matrix(NA, nrow=1,ncol=3)
+  testthat::expect_error(simulate_admixture(pop_size = 100,
+                                            number_of_founders = 2,
+                                            total_runtime = 100,
+                                            morgan = 1,
+                                            seed = 42,
+                                            select_matrix = select_matrix,
+                                            progress_bar = TRUE,
+                                            track_junctions = FALSE,
+                                            track_frequency = FALSE,
+                                            multiplicative_selection = TRUE))
+
+  track_freq <- c(0.4, 0.6, 100)
+  vx <- simulate_admixture(pop_size = 100,
+                           number_of_founders = 2,
+                           total_runtime = 100,
+                           morgan = 1,
+                           seed = 42,
+                           select_matrix = NA,
+                           progress_bar = TRUE,
+                           track_junctions = FALSE,
+                           track_frequency = track_freq,
+                           multiplicative_selection = TRUE)
+
+  vx <- simulate_admixture(pop_size = 100,
+                           number_of_founders = 2,
+                           total_runtime = 100,
+                           morgan = 1,
+                           seed = 42,
+                           select_matrix = NA,
+                           progress_bar = TRUE,
+                           track_junctions = TRUE,
+                           track_frequency = FALSE,
+                           multiplicative_selection = TRUE)
+
+
+  vx <- simulate_admixture(pop_size = 100,
+                           number_of_founders = 2,
+                           total_runtime = 100,
+                           morgan = 1,
+                           seed = 42,
+                           select_matrix = NA,
+                           progress_bar = TRUE,
+                           track_junctions = TRUE,
+                           track_frequency = track_freq,
+                           multiplicative_selection = TRUE)
+
+})
