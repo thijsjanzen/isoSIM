687ffc247a5e41e3cdf5d995c7968a530d706a07
Thijs Janzen <thijsjanzen@gmail.com>
2018-07-09 16:07:41 +0200

    added rcpp::Stop

--- src/Fish.cpp
+++ src/Fish.cpp
@@ -102,7 +102,7 @@ void Recombine(      std::vector<junction>& offspring,
     for(int i = 0; i < toAdd.size(); ++i) {
         if(toAdd[i].right == -1) {
             Rcout << "This break point was not addressed!\n";
-            exit(0);
+            Rcpp::stop();
         }
         offspring.push_back(toAdd[i]);
     }
@@ -187,7 +187,7 @@ void Recombine(      std::vector<junction>& offspring,
                 }
                 Rcout << "Do the parents have a -1 as well? (1 = yes, 0 is no)\n";
                 Rcout << "Parent1: " << parent1 << "\t" << "Parent2: " << parent2 << "\n";
-                exit(0);
+                Rcpp::stop();
             }
         }
 
