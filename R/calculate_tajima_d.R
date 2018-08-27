7e447ae64d22324cf869dc85cff53b4381587ce0
Thijs Janzen <thijsjanzen@gmail.com>
2018-08-27 22:13:36 +0200

    update tajima D

--- R/calculate_tajima_d.R
+++ R/calculate_tajima_d.R
@@ -27,19 +27,19 @@ calculate_tajima_d <- function(pop,
   expected_theta <- 4*pop_size * mutation_rate
 
   n <- 2*number_of_sampled_individuals
-  i <- 1:(n-1)
-  a1 <- sum(1 / i)
-  a2 <- sum(1 / (i^2))
-  b1 <- (n + 1) / (3 * (n - 1))
-  b2 <- 2*(n^2 + n + 3) / (9 * n * (n - 1))
-  c1 <- b1 - 1 / a1
-  c2 <- b2 - (n + 2) / (a1 * n) + a2 / (a1^2)
-  e1 <- c1 / a1
-  e2 <- c2 / (a1^2 + a2)
+  tmp <- 1:(n - 1)
+  a1 <- sum(1/tmp)
+  a2 <- sum(1/tmp^2)
+  b1 <- (n + 1)/(3 * (n - 1))
+  b2 <- 2 * (n^2 + n + 3)/(9 * n * (n - 1))
+  c1 <- b1 - 1/a1
+  c2 <- b2 - (n + 2)/(a1 * n) + a2/a1^2
+  e1 <- c1/a1
+  e2 <- c2/(a1^2 + a2)
 
   pi <- 0 #average number of pairwise differences
   cnt <- 0
-  for(i in 1:number_of_sampled_individuals) {
+  for(i in 1:length(loci_matrix[,1])) {
     for(j in 1:i) {
       if(i != j) {
         loci_1 <- loci_matrix[i,]
@@ -48,15 +48,16 @@ calculate_tajima_d <- function(pop,
         num_diff <- length(difference) - length(which(difference == 0))
         pi <- pi + num_diff
         cnt <- cnt + 1
+        #    cat(i,j,"\n")
       }
     }
   }
 
-  if(choose(number_of_sampled_individuals, 2) != cnt) {
+  if(choose(2*number_of_sampled_individuals, 2) != cnt) {
     stop("Too many pairwise comparisons!\n")
   }
 
-  pi <- pi * 1.0 / (choose(number_of_sampled_individuals, 2))
+  pi <- pi * 1.0 / (choose(2*number_of_sampled_individuals, 2))
 
   S <- 0 # number of segregating sites
   for(i in 1:length(loci_matrix[1,])) {
@@ -68,10 +69,22 @@ calculate_tajima_d <- function(pop,
 
   D = (pi - S / a1) / sqrt(e1*S + e2 * S * (S - 1))
 
+  Dmin <- (2/n - 1/a1)/sqrt(e2)
+  Dmax <- ((n + 1)/(2 * n) - 1/a1)/sqrt(e2)
+  tmp1 <- 1 + Dmin * Dmax
+  tmp2 <- Dmax - Dmin
+  a <- -tmp1 * Dmax/tmp2
+  b <- tmp1 * Dmin/tmp2
+  p <- pbeta((D - Dmin)/tmp2, b, a)
+  p <- if (p < 0.5) {
+    2 * p } else {2 * (1 - p)}
+
+
   theta_hat <- S / a1
 
   return(list("D" = D,
               "Pi" = pi,
               "S" = S,
-              "theta_hat[Estimated_from_S]" = theta_hat))
+              "theta_hat[Estimated_from_S]" = theta_hat,
+              "p value" = p))
 }
